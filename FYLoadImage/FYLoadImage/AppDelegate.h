f25544df45beb29e5ef95f9a533449949f65f527
李朝阳 <237777480@qq.com>
2016-08-21 15:56:27 +0800

    创建项目

---
+++ FYLoadImage/FYLoadImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FYLoadImage
+//
+//  Created by donotbedidouzu on 16/8/21.
+//  Copyright © 2016年 浮云. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FYLoadImage/FYLoadImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FYLoadImage
+//
+//  Created by donotbedidouzu on 16/8/21.
+//  Copyright © 2016年 浮云. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
