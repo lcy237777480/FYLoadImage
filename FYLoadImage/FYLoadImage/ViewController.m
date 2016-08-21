1ef208fb0270086fd6ce9aaf4e53ba34e3a492ad
李朝阳 <237777480@qq.com>
2016-08-21 16:00:40 +0800

    导入第三方库,请求网络JSON数据

--- FYLoadImage/FYLoadImage/ViewController.m
+++ FYLoadImage/FYLoadImage/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "AFNetworking.h"
 
 @interface ViewController ()
 
@@ -16,9 +17,27 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self loadJsonData];
 }
+/// 获取json数据的主方法
+- (void)loadJsonData
+{
+    // 创建网络请求管理者
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    
+    // json数据的地址
+    NSString *URLString = @"https://raw.githubusercontent.com/zhangxiaochuZXC/ServerFile20/master/apps.json";
+    
+    // 网络请求管理者发送GET请求,获取json数据;
+    // 默认是异步执行的,回调默认是主线程
+    [manager GET:URLString parameters:nil progress:nil success:^(NSURLSessionDataTask * _Nonnull task, NSArray *responseObject) {
+        
 
+        
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        
+    }];
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
